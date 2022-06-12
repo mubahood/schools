5eeb0b74f5e2fe0121be820e6f5eba9264d7e1c4
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-06-12 12:08:07 +0300

    schools

--- vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
+++ vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
@@ -2,17 +2,17 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        @if(config('admin.show_environment'))
+        @if (config('admin.show_environment'))
             <strong>Env</strong>&nbsp;&nbsp; {!! config('app.env') !!}
         @endif
 
         &nbsp;&nbsp;&nbsp;&nbsp;
 
-        @if(config('admin.show_version'))
-        <strong>Version</strong>&nbsp;&nbsp; {!! \Encore\Admin\Admin::VERSION !!}
+        @if (config('admin.show_version'))
+            <strong>Version</strong>&nbsp;&nbsp; {!! \Encore\Admin\Admin::VERSION !!}
         @endif
 
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="javscript:;" >Newline Technologies</a></strong>
+    <strong>Powered by <a href="javscript:;">Newline Technologies</a></strong>
 </footer>
