1034dafb7d2ed2af8f28073caa02054e903c3c67
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-06-18 20:34:39 +0300

    anjane

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ApiController; 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', [ApiController::class, 'login']);
+Route::post('login', [ApiController::class, 'login']);//new
 
 /* Route::group(['middleware' => 'api'], function ($router) {
     Route::post('/register', [ApiController::class, 'register']);
