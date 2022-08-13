bca511c41089453a7a3aabea942ee40a6e6e1312
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-13 17:23:36 +0300

    done

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('git', function (Request $r) {
     $resp = shell_exec('git config pull');
+    $resp = shell_exec('git fetch --all');
 
     echo "<pre>";
     print_r($resp);
 
-    die("this is done here! Test new !!!!!!!");
+    die("DOne magic");
+    
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
