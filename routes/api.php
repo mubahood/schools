aa29aae61a98591dc3ae1892b5f6716f2f913421
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-15 19:58:24 +0300

    anjen

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('git', function (Request $r) {
-    $resp = shell_exec('git pull --rebase=interactive -s recursive -X theirs');  
+    //$resp = shell_exec('git pull --rebase=interactive -s recursive -X theirs');  
+    $resp = shell_exec('git commit --romina');  
+    $resp = shell_exec('git pull');  
  
 
     echo "<pre>";
