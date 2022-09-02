43d2bd1d8e6bb4bdacdcbf0a0511a80ab760fe74
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-09-02 10:25:01 +0300

    romina

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,13 +1,140 @@
-<div>
-    <img src="public/assets/icons/logo.png">
-    <img class="float-right" src="public/assets/icons/badge.png">
-</div>
-<div class="p-5 border border-5 border-primary m-1 mt-5 rounded bg-light">
+<style>
+    .cpanel-tab {}
+
+    .text-dark {
+        color: black;
+    }
+
+    .divider {
+        background-color: black;
+        height: 1px;
+        margin-bottom: 1rem;
+        margin-left: 1.7rem;
+        margin-right: 2rem;
+
+    }
+</style>
+<div class="p-5 border border-5 border-primary m-1 mt-0 rounded bg-light">
+
     <div class="row border-primary border-bottom  p-0">
-        <div class="col-4 p-0 m-0"></div>
         <div class="col-4 p-0 m-0">
-            <h2 class="text-center p-0 m-0 bg-primary rounded-top">CONTROL PANEL</h2>
+            {{-- <img src="public/assets/icons/logo.png"> --}}
         </div>
-        <div class="col-4"></div>
+        <div class="col-4  m-0 cpanel-tab ">
+            <h2 class="text-center pt-3 pb-1 m-0 bg-primary rounded-top h3">CONTROL PANEL</h2>
+        </div>
+        <div class="col-4">
+            {{-- <img class="float-right" src="public/assets/icons/badge.png"> --}}
+        </div>
+    </div>
+    <div class="row  mt-3 ">
+        <div class="col-md-6 pl-0 pr-1">
+            <div class=" border border-primary m-0 rounded">
+
+                <div>
+                    <div class="row ">
+                        <div class="col-3">
+                            <div class="border border-primary m-4  rounded mr-0 p-2 mb-0">
+                                <img class="img-fluid" src="public/assets/icons/student.png">
+                            </div>
+                        </div>
+                        <div class="col-8  p-0 pt-1">
+                            <h2 class="h2 text-dark mb-0 p-0 mb-1" style="line-height: 1.5rem;">Admission & Classes
+                                Centre</h2>
+                            <p class=" m-0">Manages Subjects Bio Data, Class lists, promotions</p>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                </div>
+
+                <div>
+                    <div class="row ">
+                        <div class="col-3">
+                            <div class="border border-primary m-4  rounded mr-0 p-2 mb-0">
+                                <img class="img-fluid" src="public/assets/icons/student.png">
+                            </div>
+                        </div>
+                        <div class="col-8  p-0 pt-1">
+                            <h2 class="h2 text-dark mb-0 p-0 mb-1" style="line-height: 1.5rem;">
+                                Accounting and Finance</h2>
+                            <p class=" m-0">Manages acconts receivables and payables, Financial analytics and
+                                documentation</p>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                </div>
+
+                <div>
+                    <div class="row ">
+                        <div class="col-3">
+                            <div class="border border-primary m-4  rounded mr-0 p-2 mb-0">
+                                <img class="img-fluid" src="public/assets/icons/student.png">
+                            </div>
+                        </div>
+                        <div class="col-8  p-0 pt-1">
+                            <h2 class="h2 text-dark mb-0 p-0 mb-1" style="line-height: 1.5rem;">Admission & Classes
+                                center</h2>
+                            <p class=" m-0">Manages Subjects Bio Data, Class lists, promotions</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-6 pl-0 pr-1">
+            <div class=" border border-primary m-0 rounded">
+
+                <div>
+                    <div class="row ">
+                        <div class="col-3">
+                            <div class="border border-primary m-4  rounded mr-0 p-2 mb-0">
+                                <img class="img-fluid" src="public/assets/icons/student.png">
+                            </div>
+                        </div>
+                        <div class="col-8  p-0 pt-1">
+                            <h2 class="h2 text-dark mb-0 p-0 mb-1" style="line-height: 1.5rem;">Admission & Finance
+                                Centre</h2>
+                            <p class=" m-0">Manages acconts receivables and payables, Financial analytics and
+                                documentation</p>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                </div>
+
+                <div>
+                    <div class="row ">
+                        <div class="col-3">
+                            <div class="border border-primary m-4  rounded mr-0 p-2 mb-0">
+                                <img class="img-fluid" src="public/assets/icons/student.png">
+                            </div>
+                        </div>
+                        <div class="col-8  p-0 pt-1">
+                            <h2 class="h2 text-dark mb-0 p-0 mb-1" style="line-height: 1.5rem;">Admission & Classes
+                                center</h2>
+                            <p class=" m-0">Manages Subjects Bio Data, Class lists, promotions</p>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                </div>
+
+                <div>
+                    <div class="row ">
+                        <div class="col-3">
+                            <div class="border border-primary m-4  rounded mr-0 p-2 mb-0">
+                                <img class="img-fluid" src="public/assets/icons/student.png">
+                            </div>
+                        </div>
+                        <div class="col-8  p-0 pt-1">
+                            <h2 class="h2 text-dark mb-0 p-0 mb-1" style="line-height: 1.5rem;">Admission & Classes
+                                center</h2>
+                            <p class=" m-0">Manages Subjects Bio Data, Class lists, promotions</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </div>
