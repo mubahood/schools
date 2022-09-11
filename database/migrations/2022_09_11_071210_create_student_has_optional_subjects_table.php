c652668613b71a0386dbe89e86e0c5ff3b3b9f38
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-09-11 10:14:50 +0300

    romina

---
+++ database/migrations/2022_09_11_071210_create_student_has_optional_subjects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\AcademicClass;
+use App\Models\Course;
+use App\Models\MainCourse;
+use App\Models\StudentHasClass;
+use Encore\Admin\Auth\Database\Administrator;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentHasOptionalSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_has_optional_subjects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps(); 
+            $table->foreignIdFor(AcademicClass::class);
+            $table->foreignIdFor(Course::class);
+            $table->foreignIdFor(MainCourse::class);
+            $table->foreignIdFor(Administrator::class);
+            $table->foreignIdFor(StudentHasClass::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_has_optional_subjects');
+    }
+}
