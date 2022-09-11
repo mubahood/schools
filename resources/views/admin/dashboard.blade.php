311dfe8e6109607cde07d7954d5190ba1d254f14
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-09-11 23:47:42 +0300

    romina

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@ if (!isset($menu_items)) {
 }
 ?><div class="row">
     @foreach ($menu_items as $item)
-        <div class="col-6 col-md-3">
+        <div class="col-6 col-md-3"> 
             @include('widgets.box-3', [
                 'title' => $item->title,
                 'sub_title' => $item->sub_title,
