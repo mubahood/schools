8ec47f8f1effaded8d8b397595f071d97c2b3d4b
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-09-16 23:10:31 +0300

    romina

--- vendor/encore/laravel-admin/resources/views/login.blade.php
+++ vendor/encore/laravel-admin/resources/views/login.blade.php
@@ -99,7 +99,7 @@ $ent = Utils::ent();
 <body class="">
     <div class="row">
         <div class="col-md-6 fill pc-only " style="background-color: {{ $ent->color }}">
-            <img class="img-fluid center " width="25%" src="{{ url($ent->logo) }}" alt="">
+            <img class="img-fluid center " width="25%" src="{{ url("storage/$ent->logo") }}" alt="">
 
             <div class="description">
                 <h2>{{ $ent->name }}.</h2>
@@ -122,7 +122,7 @@ $ent = Utils::ent();
                     <a href="{{ admin_url('/') }}"><b>{{ config('admin.name') }}</b></a>
                 </div> --}}
 
-                <img class="img-fluid center mobo-only " width="30%" src="{{ url($ent->logo) }}" alt="">
+                <img class="img-fluid center mobo-only " width="30%" src="{{ url("storage/$ent->logo") }}" alt="">
 
                 <div class="login-logo">
                     {{-- <h2>Log in to your account</h2> --}}
