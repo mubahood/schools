355f0015792d36cdabb2645904ad09019bd0ccc4
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-10-02 03:45:30 +0300

    images

--- vendor/encore/laravel-admin/src/Auth/Database/Administrator.php
+++ vendor/encore/laravel-admin/src/Auth/Database/Administrator.php
@@ -129,19 +129,13 @@ class Administrator extends Model implements AuthenticatableContract
      */
     public function getAvatarAttribute($avatar)
     {
-        if (url()->isValidUrl($avatar)) {
-            return $avatar;
+
+        if($avatar == null || strlen($avatar)<3 ){
+            $default = config('admin.default_avatar') ?: '/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg';
+
         }
+        return url('storage/'.$avatar);
 
-        $disk = config('admin.upload.disk');
-
-        if ($avatar && array_key_exists($disk, config('filesystems.disks'))) {
-            return Storage::disk(config('admin.upload.disk'))->url($avatar);
-        }
-
-        $default = config('admin.default_avatar') ?: '/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg';
-
-        return admin_asset($default);
     }
 
     /**
