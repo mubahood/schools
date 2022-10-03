bc1e7f31e55a3c802f4ccfbf207a4194f9f166df
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-10-03 15:50:05 +0300

    romina

--- app/Models/UserBatchImporter.php
+++ app/Models/UserBatchImporter.php
@@ -437,22 +437,24 @@ class UserBatchImporter extends Model
             $u->nssf_number = trim($v[21]);
             $u->user_type = 'employee';
             $u->save();
-            print($u->id."<hr>");
+            
 
             $sql = "SELECT * FROM admin_role_users WHERE role_id = 5 AND user_id = $u->id";
 
-            dd($sql);
             $_role = DB::select($sql);
             if($_role == null){
                 $_role = [];
             }
             if (count($_role)  == 0) {
+ 
                 DB::insert("INSERT INTO admin_role_users (
                     role_id,user_id
                 )VALUES(
                     5,
-                    
+                    $u->id
                 )");
+
+                die("scucesss");
             }
         }
         $m->description = "Imported $import_count new employees and Updated $update_count employees.";
