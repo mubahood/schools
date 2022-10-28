b78af5b4b57ba0773b42142362c55b45818c98b6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-10-28 20:18:24 +0300

    bootstrap

--- resources/views/report-cards/print.blade.php
+++ resources/views/report-cards/print.blade.php
@@ -2,12 +2,11 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link type="text/css" href="{{ url('assets/bootstrap.css') }}" rel="stylesheet" />
-    <link type="text/css" href="{{ url('assets/print.css') }}" rel="stylesheet" />
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
 </head>
 
 <body>
