880410df6d3e085ee4c1f0f60e4f0e86fad44b1d
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-12-16 01:46:28 +0300

    $u

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -142,7 +142,7 @@ class HomeController extends Controller
             $u->isRole('bursar')
         ) {
             $u = Auth::user();
-
+            dd($u);
             $content
                 ->title($u->ent->short_name . ' - Dashboard')
                 ->description('Hello ' . $u->name . "!");
