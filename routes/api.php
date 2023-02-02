cae7397b435c2f21280c0f3a0b96db19364bb117
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-02-03 02:30:54 +0300

    token

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,12 @@ use App\Models\Utils;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::POST("users/register", [ApiAuthController::class, "register"]);
-Route::POST("login", [ApiAuthController::class, "login"]);
+Route::POST("users/login", [ApiAuthController::class, "login"]);
+
+Route::group(['middleware' => 'api'], function ($router) {
+    Route::get("users/me", [ApiAuthController::class, 'me']);
+});
 
 
 
