8cb6470c32fb0e409c0cfacb10a6fec9974210f1
Muhindo Mubaraka <mubs0x@gmail.com>
2023-05-03 13:39:11 +0300

    romina

--- app/Admin/Controllers/StudentReportCardController.php
+++ app/Admin/Controllers/StudentReportCardController.php
@@ -180,7 +180,7 @@ class StudentReportCardController extends AdminController
      */
     protected function grid()
     {
-     /*  
+        /* 
         set_time_limit(-1);
         ini_set('memory_limit', '-1');
         foreach (StudentReportCard::all() as $c) {
@@ -210,7 +210,6 @@ class StudentReportCardController extends AdminController
         $r = TermlyReportCard::find(3);
         $r::grade_students($r);
         dd($r);
-
 
         die("simple test"); */
         $grid = new Grid(new StudentReportCard());
