377edf3155c40b554c3340316025ef7944b5319e
Muhindo Mubaraka <mubs0x@gmail.com>
2023-09-14 11:24:18 +0300

    has_valid_lisence

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -39,7 +39,11 @@ use Illuminate\Support\Facades\DB;
 Encore\Admin\Form::forget(['map', 'editor']);
 
 $u = Auth::user();
-
+if ($u->ent != null) {
+    if ($u->ent->has_valid_lisence != 'Yes') {
+        die('License for <b>' . $u->ent->name . '</b> has expired. Please contact, <b>Newline Technologies Ltd</b>. for renewal.');
+    }
+}
 
 if ($u != null) {
     Utils::system_boot($u);
