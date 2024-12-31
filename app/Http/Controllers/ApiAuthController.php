9101f4979c118478c4da6d98f377c44cf310b6d6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-12-31 08:42:03 +0300

    Improve error message for duplicate user registration to include the phone number in ApiAuthController

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -165,7 +165,7 @@ class ApiAuthController extends Controller
 
         $u = Administrator::where('phone_number_1', $phone_number)->first();
         if ($u != null) {
-            return $this->error('User with same phone number  as phone number already exists.');
+            return $this->error('User with same phone number  as phone number already exists. (' . $phone_number . ')');
         }
         $u = Administrator::where('username', $phone_number)->first();
         if ($u != null) {
