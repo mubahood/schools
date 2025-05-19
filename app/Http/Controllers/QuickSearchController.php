6bbe71311dc61aacce1f5498c15964fca6ab5e5e
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-05-19 14:32:31 +0300

    Fix current class text formatting in studentsFinancialAccounts method

--- app/Http/Controllers/QuickSearchController.php
+++ app/Http/Controllers/QuickSearchController.php
@@ -73,7 +73,7 @@ class QuickSearchController extends Controller
                 $user->current_class_text = $user->current_class_id;
                 $class = $user->getActiveClass();
                 if ($class != null) {
-                    $current_class_text = " - " . $class->short_name . " - (Bal: " . number_format($val->balance) . ")";
+                    $current_class_text = $user->name . " - (Bal: " . number_format($val->balance) . ")";
                 }
             }
             $data[] = [
