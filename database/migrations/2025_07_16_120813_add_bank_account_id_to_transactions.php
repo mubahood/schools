7fa48e62a6d77ea997379fa4f76ef409e63ac107
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-07-16 12:09:18 +0300

    feat: Add bank account ID to transactions migration for improved transaction management

---
+++ database/migrations/2025_07_16_120813_add_bank_account_id_to_transactions.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\BankAccount;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBankAccountIdToTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->foreignIdFor(BankAccount::class)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            //
+        });
+    }
+}
